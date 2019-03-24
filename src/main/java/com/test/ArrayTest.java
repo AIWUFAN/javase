acd5a21555b9f7c05b7b2a8dba6e91ae708e263a
AIWUFAN <git@github.com:AIWUFAN>
2019-03-24 15:54:05 +0800

    mapTest方法测试

---
+++ src/main/java/com/test/ArrayTest.java
@@ -0,0 +1,28 @@
+package com.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArrayTest {
+    public static void main(String[] args) {
+        mapTest();
+    }
+
+    public static void mapTest(){
+//        Map<String, String> map = new HashMap<>();
+//        map.put("1","a");
+//        map.put("2","b");
+//        map.put("3","c");
+//        for (Map.Entry<String,String> mm: map.entrySet()) {
+//            System.out.println(map);
+//        }
+      //  System.out.println(map.containsKey("1"));
+
+        Integer a =1;
+        Integer b =1;
+        System.out.println(a==b);
+        Integer integer = Integer.valueOf(1);
+        Integer intege2 = Integer.valueOf(1);
+        System.out.println(integer==intege2);
+    }
+}
